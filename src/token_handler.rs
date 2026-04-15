46487b81b9f45f9a4f9b98235aeabe4389ec51ee
JakeSiewJK64 <global.strenuous055@passinbox.com>
2026-04-15 09:47:23 +0800

    token handler remove crate dependency for fs

--- src/token_handler.rs
+++ src/token_handler.rs
@@ -2,7 +2,7 @@ pub fn save_token(token: &str) -> Result<(), std::io::Error> {
     let mut clickdown_folder_path = crate::PathBuf::from(crate::utils::get_home_dir());
     clickdown_folder_path.push(".config/clickdown/token");
 
-    match crate::fs::write(clickdown_folder_path, token) {
+    match std::fs::write(clickdown_folder_path, token) {
         Ok(value) => {
             println!("Saved token at ~/.config/clickdown");
             value
@@ -20,10 +20,10 @@ pub fn get_clickdown_token() -> Result<String, std::io::Error> {
     clickdown_folder_path.push(".config/clickdown");
 
     // create clickdown .config folder if missing
-    crate::fs::create_dir_all(&clickdown_folder_path)?;
+    std::fs::create_dir_all(&clickdown_folder_path)?;
 
     // append filename
     clickdown_folder_path.push("token");
 
-    Ok(crate::fs::read_to_string(clickdown_folder_path).unwrap_or("".to_string()))
+    Ok(std::fs::read_to_string(clickdown_folder_path).unwrap_or("".to_string()))
 }
